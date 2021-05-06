0d1f6cdb6d277ebcc7285dff9f45ed7b7ccc3a17
Indra Palijama <indrapalijama@gmail.com>
2021-05-07 06:39:36 +0700

    fixing heroku cors

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.options('*', cors()) // include before other routes
 app.use(cors());
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "localhost" + process.env.PORT); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "localhost:" + process.env.PORT); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
