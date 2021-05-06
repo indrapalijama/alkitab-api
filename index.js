ba4d25e58867aa845d3a19abbaa10c43e8d1f701
Indra Palijama <indrapalijama@gmail.com>
2021-05-07 05:58:44 +0700

    fixing heroku cors

--- index.js
+++ index.js
@@ -143,13 +143,16 @@ app.get('/read/:book/:chapter/:version', (req, res) => {
     })
 })
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+//     next();
+// });
 
+app.configure(function () {
+    app.use(allowCrossDomain);
+});
 
 app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
